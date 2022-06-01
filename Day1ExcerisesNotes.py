1b6cf549a3295eefbbf58eb7ddd6a583b7581728
aqdasilva <a6dasilva@student.bridgew.edu>
2022-06-01 18:10:22 -0400

    day1 Coding with Python using Udemy Course learned strings,input,variables,len made project to combine strings count the characters in a string

---
+++ Day1ExcerisesNotes.py
@@ -0,0 +1,33 @@
+# firs line of code
+# simple print hello world
+print("Hello World, this is day 1 of 100 days")  # this is a string
+
+# print new line
+print("Hello World !:D \nDay 1 of Coding out of 100")
+
+# add strings together
+print("Hello" + " " + "World")
+
+# getting user Input
+print("Hello " + input("What is your name?\n"))
+
+# day1 Practice
+# prints number of characters in a users name
+print(len(input("What is your name?\n")))
+
+#printing variables
+name = input("What is your name today?")
+length = len(name)
+print(length)
+
+#day1 final excerise: switches the values stored in the variables a and b
+a = input("a: ")
+b = input("b: ")
+
+tempVar = a
+a = b
+b = tempVar
+
+print("a = " + a)
+print("b = " + b)
+
---
+++ Day1Project.py
@@ -0,0 +1,12 @@
+# day1Project
+# 1 Create a greeting
+print("Greetings from Mars")
+
+# 2 ask user for the city they gre up in
+birth_city = input("What is the city that you grew up in?\n")
+print(birth_city)
+# 3ask user for name of a pet
+pets_name = input("What is the name of your pet?\n")
+print(pets_name)
+# 4 combine pet and city
+print("Your band name could be " + birth_city + " " + pets_name)
