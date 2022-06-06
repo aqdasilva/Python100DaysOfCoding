13d534713dc340729380745d1cbd7e35f721cd97
aqdasilva <a6dasilva@student.bridgew.edu>
2022-06-06 09:52:16 -0400

    day6: learned: while loops, functions used: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
    all exceries done on this site and project

---
+++ ExceriesNotes/Day6ExceriesNotes.py
@@ -0,0 +1,37 @@
+# Day6 Functions, CodeBlocks, While Loops
+
+def myFunction():
+    print("hello")
+    print("bye")
+
+myFunction()
+
+# reeborgs world using functions
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# def turnRight():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def oneHurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turnRight()
+#     move()
+#     turnRight()
+#     move()
+#     turn_left()
+#
+# for step in range(6):
+#     oneHurdle()
+
+# #for step in range(6):
+#     oneHurdle()
+#
+# while not at_goal():
+#     oneHurdle()
+
+# For Loops: are great for iterating through a list
+
+
---
+++ Projects/Day6Project.py
@@ -0,0 +1,14 @@
+#used reeborg for day6 project
+# def turnRight():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turnRight()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
